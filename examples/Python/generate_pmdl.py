a9895ae83d4e58a47c59643b0697a96418d60d95
jln-ho <howes.julian@gmail.com>
2021-03-02 22:22:48 +0100

    Change command line arguments to allow for an arbitrary number of audio samples to be used

--- examples/Python/generate_pmdl.py
+++ examples/Python/generate_pmdl.py
@@ -23,10 +23,9 @@ def check_enroll_output(enroll_ans):
 
 def main():
     parser = argparse.ArgumentParser(description='Command line client for generating snowboy personal model')
-    parser.add_argument('-r1', '--record1', dest="record1", required=True, help="Record voice 1")
-    parser.add_argument('-r2', '--record2', dest="record2", required=True, help="Record voice 2")
-    parser.add_argument('-r3', '--record3', dest="record3", required=True, help="Record voice 3")
-    parser.add_argument('-n', '--name', dest="model_name", required=True, help="Personal model name")
+    parser.add_argument('-m', '--model', dest="model_file", required=True, help="Model output file")
+    parser.add_argument('-s', '--samples', dest="samples", required=True, type=argparse.FileType('r'), nargs='+',
+                        help="PCM audio files (mono, 16kHz, 16 bit)")
     parser.add_argument('-lang', '--language', default="en", dest="language", help="Language")
     args = parser.parse_args()
 
@@ -47,20 +46,18 @@ def main():
     assert cut.BitsPerSample() == enroll.BitsPerSample()
     print("channels: %d, sample rate: %d, bits: %d" % (cut.NumChannels(), cut.SampleRate(), cut.BitsPerSample()))
 
-    recording_set = [args.record1, args.record2, args.record3]
-    for rec in recording_set:
-        print("processing %s" % rec)
-        _, data = wavfile.read(rec)
+    for sample in args.samples:
+        print("processing %s" % sample.name)
+        _, data = wavfile.read(sample)
         data_cut = cut.CutTemplate(data.tobytes())
         enroll_ans = enroll.RunEnrollment(data_cut)
 
     check_enroll_output(enroll_ans)
 
-    filename = args.model_name
-    print("saving file to %s" % filename)
-    f = open(filename, "wb")
-    f.write(open(out.name).read())
-    f.close()
+    filename = args.model_file
+    print("saving model file to %s" % filename)
+    with open(filename, "wb") as f:
+        f.write(open(out.name).read())
     print("finished")
 
 
